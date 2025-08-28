d85d09dc2ee53ae778b488861224040682d0c933
Vismil <vismilgu@gmail.com>
2025-08-28 17:50:35 -0400

    Para presentar

--- PilaStruc/PilaStruc.cpp
+++ PilaStruc/PilaStruc.cpp
@@ -1,160 +1,184 @@
+/*
+   Tarea 2. Pila
+
+    Una pila (stack) es una estructura de datos en donde el último en entrar es el primero en salir.
+    Construir un programa C++ que simule una pila, utilizando una estructura de datos como la que
+    sigue para los nodos:
+    struct Pila {
+    int dato;
+    Pila *next;
+    };
+
+
+
+   Autores: Vismil Garcia - 1128719, Jeremy Garcia - 1127276, Hesler Cuevas - 1127855, Diana Lantigua - 1126796, Josephine Rosario - 1127597 e Ivan Joa - 1127943.
+
+   Fecha: 28/8/2025
+
+*/
+
 #include <iostream>
+#include <limits>
 
 using namespace std;
-//estructura de nodo
-struct Nodo
-{
-    int val;
-    Nodo* next;
-    Nodo(int valor) {
-        val = valor;
-        next = nullptr;
-    }
+
+//Pila
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
 };
-//declariacion de funciones
-int getint();
-void push(Nodo*& pila, int val);
-void pop(Nodo *&stack);
-void mostrar(Nodo *&stack, bool vaciar);
-void pause();
-int main()
-{
-    Nodo* stack = NULL;
-    bool salir = false;
-    int val = 0;
-    int tope = 0;
-    while(!salir)
-    {
-        cout << "\033[2J\033[1;1H";
-        cout << "Opciones de la pila\n";
-        cout << "1.push\n2.pop\n3.deplegar todos los elementos\n4.salir"<<endl;
-        cout << "\nCantidad de elementos en la pila: " << tope << endl;
-        mostrar(stack, false);
-        cout << right;
-        cout << "\nSeleccione la accion que quiere realizar\n";
-        
-        int opcion = getint();
-        if (opcion < 1 || opcion > 4)
-        {
-            cout << "El numero debe estar entre el rango de opciones.\n";
-            continue;
-        }
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        switch (opcion)
-        {
-        case 1:
-            cout << "\nQue numero quieres insertar a la pila?\n ";
-            val = getint();
-            push(stack, val);
-            tope++;
-            pause();
-            break;
-        case 2:
-            if (tope == 0)
-            {
-                cout << "La cola esta vacia\n";
-                pause();
-                break;
-            }
-            cout << endl;
-            pop(stack);
-            tope--;
-            pause();
-            break;
+int sizePila = 0;
 
-        case 3:
-            if (tope == 0)
-            {
-                cout << "La cola esta vacia\n";
-                pause();
-                break;
-            }
-            cout << endl;
-            mostrar(stack, true);
-            tope = 0;
-            pause();
-            break;
+//Prototipo de función pilas
+void push(Nodo*& pila, int n);
+int top(Nodo*& pila);
+void pop(Nodo*& pila);
+bool empty(Nodo*& pila);
+int size();
 
-        case 4:
-            cout << "Gracias por usar!\n";
-            pause();
-            salir = true;
+//Prototipo demás funciones
+void desplegarMenu();
+int pedirNumero(int MIN, int MAX);
+
+int main() {
+    Nodo* pila = NULL;
+
+    bool termino = false;
+    while (!termino) {
+        desplegarMenu();
+
+        int opcion = pedirNumero(1, 5);
+
+        switch (opcion) {
+        case 1: {
+            //insertar en la pila
+            int num = pedirNumero(INT_MIN, INT_MAX);
+
+            push(pila, num);
+            sizePila++;
+
+            cout << "Numero agregado exitosamente!\n\n";
+            system("pause");
             break;
         }
-        
+        case 2: {
+            //imprimir tope
+            if (empty(pila))
+                cout << "La pila esta vacia\n\n";
+            else
+                cout << "El tope es: " << top(pila) << "\n\n";
 
+            system("pause");
+            break;
+        }
+        case 3: {
+            //eliminar tope
+            if (empty(pila))
+                cout << "La pila esta vacia\n\n";
+            else {
+                int tope = top(pila);
+                pop(pila);
+                sizePila--;
+                cout << "Tope eliminado exitosamente!\n";
+                cout << "Valor eliminado: " << tope << "\n\n";
+            }
 
+            system("pause");
+            break;
+        }
+        case 4: {
+            //imprimir toda la pila
+            if (empty(pila))
+                cout << "La pila esta vacia\n\n";
+            else {
+                int ind = 1;
+                while (!empty(pila)) {
+                    cout << "[" << ind << "] " << top(pila) << "\n";
+                    pop(pila);
+                    ind++;
+                    sizePila--;
+                }
+            }
 
-
+            system("pause");
+            break;
+        }
+        case 5: {
+            //cerrar el programa
+            cout << "\nGracias por utilizar nuestra pila!\n";
+            termino = true;
+            break;
+        }
+        }
     }
 }
-int getint()
+
+//Funciones pila
+void push(Nodo*& pila, int n) {
+    Nodo* nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = n;
+    nuevo_nodo->siguiente = pila;
+
+    pila = nuevo_nodo;
+}
+
+int top(Nodo*& pila) {
+    return pila->dato;
+}
+
+void pop(Nodo*& pila) {
+    Nodo* aux = pila;
+    pila = aux->siguiente;
+    delete aux;
+}
+
+bool empty(Nodo*& pila) {
+    if (pila == NULL)
+        return true;
+
+    return false;
+}
+
+int size() {
+    return sizePila;
+}
+
+//Demás funciones
+void desplegarMenu() {
+    cout << "\033[2J\033[1;1H";
+    cout << "Opciones de la pila\n";
+    cout << "1.Push\n2.Top\n3.Pop\n4.Deplegar todos los elementos\n5.Salir" << "\n\n";
+    cout << "Cantidad de elementos en la pila: " << size() << "\n\n";
+}
+
+int pedirNumero(int MIN, int MAX)
 {
     int numero = 0;
     bool valido = false;
-    while (!valido) 
+    while (!valido)
     {
-        cout << "Introduza el numero: ";
+        if (MAX == 5) //Si max es igual a 5 significa que queremos que el user ingrese para el menu
+            cout << "Ingrese una de las opciones (1-5): ";
+        else //De lo contrario queremos que ingrese el número a agregar a la pila
+            cout << "Introduzca el numero entero a agregar: ";
+
         cin >> numero;
         if (cin.fail())
         {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Solo introduza numeros";
-            continue;
+            cout << "Solo introduza numeros\n";
         }
         else if (cin.peek() != '\n')
         {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "No agregue caraccteres extra\n";
-            continue;
+            cout << "No agregue caracteres extra\n";
         }
-        return numero;
+        else if ((numero < MIN || numero > MAX)) {
+            cout << "Solo escribir numeros entre " << MIN << " y " << MAX << "\n";
+        }
+        else
+            valido = true;
     }
     return numero;
-}
-void push(Nodo*& stack, int val)
-{
-    Nodo* nuevo_nodo = new Nodo(val);
-    nuevo_nodo->next = stack;
-    stack = nuevo_nodo;
-    cout << "\nNodo isnertado correctamente.\n";
-}
-void pop(Nodo *&stack)
-{
-    Nodo *temp = stack;
-    stack = temp->next;
-    cout << temp->val<<" \n";
-    delete temp;
-}
-
-void mostrar(Nodo*& stack, bool vaciar)
-{
-    int i = 1;
-    if (stack == nullptr) return;
-    Nodo* temp = stack;
-    if (vaciar) {
-        while (stack != nullptr)
-        {
-            cout << i << "- ";
-            pop(stack);
-            i++;
-        }
-
-    }
-    else
-    {
-        while (temp != nullptr) {
-            cout << i << "- " << temp->val << endl;
-            temp = temp->next;
-            i++;
-        }
-        delete temp;
-    }
-
-}
-void pause()
-{
-    cout << "Presiona enter para continuar...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
